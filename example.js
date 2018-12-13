13c07215b0565062d6a1643d5dbd62a16441c030
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-12-13 18:32:05 +0100

    initial

---
+++ example.js
@@ -0,0 +1,45 @@
+/*
+  runtime errors happen after creation phase/parsing
+    they will appear under the log --- broken code ---
+  syntax errors will not allow JS to parse your code
+    they will appear all alone since the logs never got to happen
+*/
+
+console.log('--- --- missing opening bracket --- ---');
+
+console.log('--- broken code ---')
+
+  console.log(`
+    let value = 0;
+
+    while (value < 9) 
+      value++;
+    };
+
+    console.assert(value === 9, 'nopes');
+  `)
+
+console.log('--- the error ---')
+
+  console.log(`
+    unknown: Unexpected token (17:4)
+      15 |     while (value < 9) 
+      16 |       value++;
+    > 17 |     };
+        |     ^
+      18 | 
+      19 |     console.assert(value === 9, 'nopes');
+  `)
+
+console.log('--- the fix ---')
+
+  console.log(`
+    let value = 0;
+
+    while (value < 9) {
+      value++;
+    };
+
+    console.assert(value === 9, 'nopes');
+  `)
+  console.log('there was a missing opening bracket after the condition check')
---
+++ template.js
@@ -0,0 +1,33 @@
+/*
+  runtime errors happen after creation phase/parsing
+    they will appear under the log --- broken code ---
+  syntax errors will not allow JS to parse your code
+    they will appear all alone since the logs never got to happen
+*/
+
+console.log('--- --- name this once you get it --- ---');
+
+console.log('--- broken code ---')
+
+  // console.log(`
+    let value = 0;
+
+    while (value < 9) 
+      value++;
+    };
+
+    console.assert(value === 9, 'nopes');
+  // `)
+
+console.log('--- the error ---')
+
+  console.log(`
+    paste in the error log from replit
+  `)
+
+console.log('--- the fix ---')
+
+  // console.log(`
+    paste in your fix
+  // `)
+  console.log('short description of your fix')
