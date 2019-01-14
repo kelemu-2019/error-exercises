3fa9237af3f376da475f99274fb51e382ccbb8dd
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-01-14 15:56:46 +0100

    some-exercises

---
+++ exercises.js
@@ -0,0 +1,59 @@
+// group them by error type or by syntactic similarity?
+
+var = 5;
+
+
+let a = {b:3};
+a.b.3
+
+
+let a = {b:'e'};
+b.e
+
+
+let a;
+a.b;
+
+
+let a = 'this is 
+two lines';
+
+
+let a = 1:
+
+
+let myArray = [1, 2, 3;
+
+
+function getNine {
+  let x = 6;
+  let y = 3;
+  return x + y;
+}
+let result = getNine;
+
+
+let innerHtml = "<p>Click here to <a href="#Home">return home</a></p>";
+
+
+let nested_messages = 'remind yourself ''i can do this!'' at least once a day';
+
+
+const a = 9;
+a = 0;
+
+
+const a;
+a = 0;
+
+
+let value = 0;
+while (value < 9) 
+  value++;
+};
+
+
+let array = [];
+array.length()
+
+
